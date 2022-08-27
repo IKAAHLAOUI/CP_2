bfe881c33534ca462647cad6c60d093a880372ee
IKAAHLAOUI <macellinaroblox@gmail.com>
2022-08-27 11:05:04 +0100

    checkpoint solved

---
+++ CP_2/Assets/CP.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CP : MonoBehaviour
+{
+  
+} void CheckPoint()
+{
+    float a;
+    float b;
+    float sum;
+    sum = a + b;
+    a = 2.1f;
+    b = 4.3f;
+    return sum;
+
+    float prod;
+    prod = a * b;
+    return prod;
+
+    float div;
+    div = a / b;
+    return div;
+} 
+//I can't print AKA Debug.Log in this version because when I type it it doesn't work.
